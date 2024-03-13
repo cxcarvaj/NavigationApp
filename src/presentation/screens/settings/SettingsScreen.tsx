57c5b72b37948f4d67f611f1bae522ffb0761ae4
Carlos Carvajal <cxcarvaj@gmail.com>
2024-03-13 14:27:42 -0500

    Implementation of go back buttons

--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,9 +1,20 @@
 import {Text, View} from 'react-native';
+import {globalStyles} from '../../theme/theme';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
+import {StackActions, useNavigation} from '@react-navigation/native';
 
 export const SettingsScreen = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={globalStyles.container}>
+      <Text style={{marginBottom: 10}}>Settings Screen</Text>
+
+      <PrimaryButton label="Go back" onPress={() => navigation.goBack()} />
+
+      <PrimaryButton
+        label="Go Back to Home"
+        onPress={() => navigation.dispatch(StackActions.popToTop)}
+      />
     </View>
   );
 };
