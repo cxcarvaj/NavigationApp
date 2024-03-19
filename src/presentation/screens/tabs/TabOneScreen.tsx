97dc4e866e8c3c4ad0a1c94771168d18a31efb10
Carlos Carvajal <cxcarvaj@gmail.com>
2024-03-19 16:15:08 -0500

    Adding icons package into the project

--- src/presentation/screens/tabs/TabOneScreen.tsx
+++ src/presentation/screens/tabs/TabOneScreen.tsx
@@ -1,11 +1,13 @@
 import {Text, View} from 'react-native';
 import {HamburgerMenu} from '../../components/shared/HamburgerMenu.tsx';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export const TabOneScreen = () => {
   return (
     <View>
       <HamburgerMenu />
       <Text>TabOneScreen</Text>
+      <Icon name="rocket-outline" size={30} color="#900" />
     </View>
   );
 };
